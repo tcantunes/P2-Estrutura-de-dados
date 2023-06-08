c1f404e6b5263a6fd2fcd4befb042604d8af8eef
tcantunes <tainacantinho@hotmail.com>
2023-06-07 22:15:37 -0300

    feat: adiciona classe animal

---
+++ animal.py
@@ -0,0 +1,7 @@
+class Animal:
+    def _init_(self, tipo, idade, cor, porte, particularidade):
+        self.tipo = tipo
+        self.idade = idade
+        self.cor = cor
+        self.porte = porte
+        self.particularidade = particularidade
