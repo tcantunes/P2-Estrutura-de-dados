529bae481cea268791d2f5a2703f1ca82a733391
tcantunes <tainacantinho@hotmail.com>
2023-06-07 22:21:57 -0300

    feat: adiciona atributo raça a classe animal

--- animal.py
+++ animal.py
@@ -1,7 +1,8 @@
 class Animal:
-    def _init_(self, tipo, idade, cor, porte, particularidade):
+    def _init_(self, tipo, idade, cor, porte, raca, particularidade):
         self.tipo = tipo
         self.idade = idade
         self.cor = cor
         self.porte = porte
+        self.raca = raca
         self.particularidade = particularidade
