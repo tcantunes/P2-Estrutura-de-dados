77265fdcc134a9c3065aaa71da92380c16e4e63b
Larissa <larissa.rocha@dmslog.com>
2023-06-10 16:23:22 -0300

    add: ajustando estilo do menu

--- menu.py
+++ menu.py
@@ -8,14 +8,10 @@ def menu_principal():
     cadastro_animais = CadastroAnimais()
     cadastro_pessoas = CadastroPessoas()
 
-    print("Sistema de Cadastro e Adoção")
+    print('\n\33[1;35mSistema de Cadastro e Adoção\33[m')
     while True:
-        print("\nMenu Principal:")
-        print("1. Cadastrar novo animal")
-        print("2. Cadastrar nova pessoa interessada na adoção")
-        print("3. Pesquisar animais disponíveis")
-        print("4. Pesquisar pessoa interessada na adoção")
-        print("5. Sair")
+        print('\nMenu Principal:\n1. Cadastrar novo animal\n2. Cadastrar nova pessoa interessada na adoção\n'
+              '3. Pesquisar animais disponíveis\n4. Pesquisar pessoa interessada na adoção\n5. Sair\n')
         escolha_menu = int(input('Informe sua opção: \n'))
 
         if escolha_menu == 1:
@@ -30,8 +26,8 @@ def menu_principal():
                 print('\nOs animais disponíveis para adoção possuem as seguintes características:\n')
 
                 for animal in animais_disponiveis:
-                    print(f'Tipo: {animal.tipo}\n Idade: {animal.idade}\n Cor: {animal.cor}\n '
-                          f'Porte: {animal.porte}\n Raça: {animal.raca}\n Particularidade: {animal.particularidade}\n')
+                    print(f'Tipo: {animal.tipo}\nIdade: {animal.idade}\nCor: {animal.cor}\n'
+                          f'Porte: {animal.porte}\nRaça: {animal.raca}\nParticularidade: {animal.particularidade}\n')
             else:
                 print('\nNão há animais disponíveis para adoção.\n')
 
@@ -40,8 +36,8 @@ def menu_principal():
             if len(pessoas_disponiveis) >= 1:
                 print('\nAs pessoas adotantes cadastradas são:\n')
                 for pessoa in pessoas_disponiveis:
-                    print(f'Nome: {pessoa.nome}\n Contato: {pessoa.contato}\n Interesse pelo tipo do animal: {pessoa.interesse_tipo}\n '
-                          f'Endereço: {pessoa.endereco}\n Interesse por particularidade do animal: {pessoa.interesse_particularidade}\n')
+                    print(f'Nome: {pessoa.nome}\nContato: {pessoa.contato}\nInteresse pelo tipo do animal: {pessoa.interesse_tipo}\n'
+                          f'Endereço: {pessoa.endereco}\nInteresse por particularidade do animal:{pessoa.interesse_particularidade}\n')
             else:
                 print('\nNão há cadastros de pessoas interessadas na adoção.\n')
         
