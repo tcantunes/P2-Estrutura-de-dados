3b73680ab3f294b5b14006360da88f88c9b83075
Larissa <larissa.rocha@dmslog.com>
2023-06-10 16:03:18 -0300

    add: cadastro dinamico de adotantes

--- cadastroPessoa.py
+++ cadastroPessoa.py
@@ -5,15 +5,11 @@ class CadastroPessoas():
     def __init__(self):
         self.pessoas = []
 
-    def cadastrar_pessoa(self, nome, contato, interesse_tipo, endereco, interesse_particularidade=None):
+    def cadastrar_pessoa(self):
+        nome = input('Informe o nome do adotante: \n')
+        contato = input('Informe o contato do adotante: \n')
+        interesse_tipo = input('Informe o interesse pelo tipo do animal: \n')
+        endereco = input('Informe o endereço do adotante: \n')
+        interesse_particularidade = input('Informe o interesse da particularidade: \n(Caso não tenha, informe que não possui (NP).)\n')
         pessoa = Pessoa(nome, contato, interesse_tipo, endereco, interesse_particularidade)
         self.pessoas.append(pessoa)
-
-        # Cadastro de pessoa por entrada de dados:
-
-        pessoa_nome = input('Informe o nome do adotante: \n')
-        pessoa_contato = input('Informe o contato do adotante: \n')
-        pessoa_interesse_tipo = input('Informe o interesse pelo tipo do animal: \n')
-        pessoa_endereco = input('Informe o endereço do adotante: \n')
-        pessoa_interesse_particularidade = input('Informe o interesse da particularidade: \n')
-        self.cadastrar_pessoa(pessoa_nome, pessoa_contato, pessoa_interesse_tipo, pessoa_endereco, pessoa_interesse_particularidade)
