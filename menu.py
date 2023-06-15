52e7c2b8bbd34545882fa814a58e385ae577affc
Larissa <larissa.rocha@dmslog.com>
2023-06-15 14:18:17 -0300

    add: pesquisas pelo tipo e relatorio

--- menu.py
+++ menu.py
@@ -51,31 +51,65 @@ def menu_principal():
     return animais_disponiveis, pessoas_disponiveis
 
 
-def pesquisar_caracteristica(animais, item, begin=0, end=None):
-    lista = []
-    for posicao, animal in enumerate(animais):
-        lista.append([posicao, animal])
-    
-    if end is None:
-        end = len(lista)-1
-    if begin <= end:
-        m = (begin + end)//2
-        if lista[m][1].tipo == item:
-            return m
-        if item < lista[m][1].tipo:
-            return pesquisar_caracteristica(animais, item, begin, m-1)
-        else:
-            return pesquisar_caracteristica(animais, item, m+1, end)
-    return None
-
 animais, pessoas = menu_principal()
 
-interesse_tipo = input('Informe o interesse do TIPO do animal: ')
-tipo_encontrado = pesquisar_caracteristica(animais, interesse_tipo)
-if tipo_encontrado is not None:
-    animal_encontrado = animais[tipo_encontrado]
-    print(f'\nAnimal com este TIPO encontrado:\nTipo: {animal_encontrado.tipo}\nIdade: {animal_encontrado.idade}\nCor: {animal_encontrado.cor}\n'
-          f'Porte: {animal_encontrado.porte}\nRaça: {animal_encontrado.raca}\nParticularidade: {animal_encontrado.particularidade}\n')
-else:
-    print('Não há animais cadastrados com este TIPO específico.')
+lista_animais, lista_pessoas = animais, pessoas
 
+def pesquisa_tipo_animais(animais, item):
+    lista_animais = []
+    for posicao, animal in enumerate(animais):
+        if animal.tipo == item:
+            lista_animais.append(animal)
+    return lista_animais
+
+def pesquisa_tipo_pessoa(pessoas, item):
+    lista_pessoas = []
+    for posicao, pessoa in enumerate(pessoas):
+        if pessoa.interesse_tipo == item:
+            lista_pessoas.append(pessoa)
+    return lista_pessoas
+
+buscar_interesse = input('Deseja fazer uma busca pelo TIPO de animal de preferência? [SIM | NÃO]\n')
+if not (buscar_interesse.lower() == 'sim' or buscar_interesse.isspace()):
+    print('Busca finalizada.\n')
+else:
+    buscar_tipo = input('Informe o interesse pelo TIPO de animal: ')
+
+    animais_encontrados = pesquisa_tipo_animais(animais, buscar_tipo)
+    animais_relatorio = []
+    if animais_encontrados:
+        print(f'\nAnimais com o tipo {buscar_tipo.upper()} encontrados:')
+        for animal in animais_encontrados:
+            animais_relatorio.append(animal)
+            print(f'Tipo: {animal.tipo}\nIdade: {animal.idade}\nRaça: {animal.raca}\n')
+    else:
+        print(f'\nAnimal com o tipo {buscar_tipo.upper()} não cadastrado.\n')
+
+    pessoas_encontradas = pesquisa_tipo_pessoa(pessoas, buscar_tipo)
+    pessoas_relatorio = []  
+    if pessoas_encontradas:
+        print(f'\nPessoas interessadas no tipo {buscar_tipo.upper()} encontradas:')
+        for pessoa in pessoas_encontradas:
+            pessoas_relatorio.append(pessoa)
+            print(f'Nome: {pessoa.nome}\nContato: {pessoa.contato}\nInteresse pelo tipo: {pessoa.interesse_tipo}\n')
+    else:
+        print(f'Pessoa interessada no tipo de animal {buscar_tipo.upper()} não cadastrada.\n')
+
+    if animais_encontrados and pessoas_encontradas:
+        emitir_relatorio = input('Deseja emitir RELATÓRIO de informações entre animais disponíveis x possíveis candidatos? [SIM | NÃO]\n')
+        if not (emitir_relatorio.lower() == 'sim' or emitir_relatorio.isspace()):
+            print('Emissão de relatório recusada.')
+        else:
+            print('\n--- RELATÓRIO ---\n')
+            print('\nANIMAIS DISPONÍVEIS PARA ADOÇÃO:')
+            for animal in animais_relatorio:
+                print('-'*30)
+                print(f'Tipo: {animal.tipo}\nIdade: {animal.idade}\nCor: {animal.cor}\n'
+                    f'Porte: {animal.porte}\nRaça: {animal.raca}\nParticularidade: {animal.particularidade}\n')
+
+            print('\nPOSSÍVEIS CANDIDATOS PARA REALIZAREM A ADOÇÃO:')
+            for pessoa in pessoas_relatorio:
+                print('-'*30)
+                print(f'Nome: {pessoa.nome}\nContato: {pessoa.contato}\nInteresse pelo tipo do animal: {pessoa.interesse_tipo}\n'
+                    f'Endereço: {pessoa.endereco}\nInteresse por particularidade do animal: {pessoa.interesse_particularidade}\n')
+                
