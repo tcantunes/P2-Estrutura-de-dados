fd10965d8edf1a9d3f8c21641068cd4e66c09562
Larissa <larissa.rocha@dmslog.com>
2023-06-15 01:27:48 -0300

    feat: add pesquisa binaria para o tipo do animal

--- menu.py
+++ menu.py
@@ -7,6 +7,8 @@ from cadastroPessoa import *
 def menu_principal():
     cadastro_animais = CadastroAnimais()
     cadastro_pessoas = CadastroPessoas()
+    animais_disponiveis = cadastro_animais.animais
+    pessoas_disponiveis = cadastro_pessoas.pessoas
 
     print('\n\33[1;35mSistema de Cadastro e Adoção\33[m')
     while True:
@@ -21,7 +23,6 @@ def menu_principal():
             cadastro_pessoas.cadastrar_pessoa()
 
         elif escolha_menu == 3:
-            animais_disponiveis = cadastro_animais.animais
             if len(animais_disponiveis) >= 1:
                 print('\nOs animais disponíveis para adoção possuem as seguintes características:\n')
 
@@ -32,21 +33,49 @@ def menu_principal():
                 print('\nNão há animais disponíveis para adoção.\n')
 
         elif escolha_menu == 4:
-            pessoas_disponiveis = cadastro_pessoas.pessoas
             if len(pessoas_disponiveis) >= 1:
                 print('\nAs pessoas adotantes cadastradas são:\n')
                 for pessoa in pessoas_disponiveis:
                     print(f'Nome: {pessoa.nome}\nContato: {pessoa.contato}\nInteresse pelo tipo do animal: {pessoa.interesse_tipo}\n'
-                          f'Endereço: {pessoa.endereco}\nInteresse por particularidade do animal:{pessoa.interesse_particularidade}\n')
+                          f'Endereço: {pessoa.endereco}\nInteresse por particularidade do animal: {pessoa.interesse_particularidade}\n')
             else:
                 print('\nNão há cadastros de pessoas interessadas na adoção.\n')
         
         elif escolha_menu == 5:
-            print('Programa finalizado.')
+            print('Programa finalizado.\n')
             break
 
         else:
             print('\nInforme uma opção válida.')
+    
+    return animais_disponiveis, pessoas_disponiveis
 
 
-menu_principal()
+def pesquisar_caracteristica(animais, item, begin=0, end=None):
+    lista = []
+    for posicao, animal in enumerate(animais):
+        lista.append([posicao, animal])
+    
+    if end is None:
+        end = len(lista)-1
+    if begin <= end:
+        m = (begin + end)//2
+        if lista[m][1].tipo == item:
+            return m
+        if item < lista[m][1].tipo:
+            return pesquisar_caracteristica(animais, item, begin, m-1)
+        else:
+            return pesquisar_caracteristica(animais, item, m+1, end)
+    return None
+
+animais, pessoas = menu_principal()
+
+interesse_tipo = input('Informe o interesse do TIPO do animal: ')
+tipo_encontrado = pesquisar_caracteristica(animais, interesse_tipo)
+if tipo_encontrado is not None:
+    animal_encontrado = animais[tipo_encontrado]
+    print(f'\nAnimal com este TIPO encontrado:\nTipo: {animal_encontrado.tipo}\nIdade: {animal_encontrado.idade}\nCor: {animal_encontrado.cor}\n'
+          f'Porte: {animal_encontrado.porte}\nRaça: {animal_encontrado.raca}\nParticularidade: {animal_encontrado.particularidade}\n')
+else:
+    print('Não há animais cadastrados com este TIPO específico.')
+
